954dc07b86482f091a2956597127fe609937ea97
Cosmas Nyairo <nyairocosmas@gmail.com>
2019-11-13 13:34:53 +0300

    Trivia Questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Yello!'),);
+
+  var questionIndex = 0;
+  void answerQuestion(){
+    questionIndex=questionIndex+1;
+    print(questionIndex);
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    var questions= [
+      'Captain America’s shield is made of?',
+      'What is the name of Tony Stark’s personal butler?',
+      'Who is the director of S.H.I.E.L.D. ?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(" Marvel Trivia"),
+
+        ),
+        
+        body: Column(
+          children: [
+              Text(questions[questionIndex]),
+              RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+              RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+              RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion), 
+          ],
+        ),
+      ),
+    );
   }
 }
