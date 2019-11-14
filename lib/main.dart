8d08213df6156e73eb1b0c0ac19847f77b2a68b2
Cosmas Nyairo <nyairocosmas@gmail.com>
2019-11-14 13:23:37 +0300

    Gitacora saved 5 changed files

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyAppState extends State<MyApp>{     //underscore changes to private to b
        'answers':['Peggy Carter','Pepper Potts','Phil Coulson','Sam Wilson'
       ]},
       {'question': 'Who is Thor’s adopted sibling?',
-       'answers':['Nick Fury','Tony Stark','Captain America','Tony Potts'
+       'answers':['Odin','Loki','Captain America','Tony Potts'
       ]},
       {'question': 'Who is the Director of S.H.I.E.L.D. ?',
        'answers':['Nick Fury','Tony Stark','Captain America','Tony Potts'
